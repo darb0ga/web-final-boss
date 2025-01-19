69a69888d5253a5f5b28aeeed29e28dfb311c8ba
Darya^_^ <77808924+darb0ga@users.noreply.github.com>
2025-01-19 14:04:55 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/web-final-boss/'
-    : '/'
-}
 
 export default defineConfig({
   plugins: [
